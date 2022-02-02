b49126aec32de3112579833c4813ad90ccbc2cfc
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-02-02 11:34:45 -0500

    make sure CompositeFilter is public

--- Sources/TextFormation/CompositeFilter.swift
+++ Sources/TextFormation/CompositeFilter.swift
@@ -1,21 +1,21 @@
 import Foundation
 import TextStory
 
-struct CompositeFilter {
-    typealias SubfilterHandler = (Filter, FilterAction) -> FilterAction
+public struct CompositeFilter {
+    public typealias SubfilterHandler = (Filter, FilterAction) -> FilterAction
 
-    let filters: [Filter]
+    public let filters: [Filter]
 
-    var actionHandler: SubfilterHandler
+    public var actionHandler: SubfilterHandler
 
-    init(filters: [Filter], handler: @escaping SubfilterHandler = { $1 }) {
+    public init(filters: [Filter], handler: @escaping SubfilterHandler = { $1 }) {
         self.filters = filters
         self.actionHandler = handler
     }
 }
 
 extension CompositeFilter: Filter {
-    func processMutation(_ mutation: TextMutation, in storage: TextStoring) -> FilterAction {
+    public func processMutation(_ mutation: TextMutation, in storage: TextStoring) -> FilterAction {
         for filter in filters {
             let action = filter.processMutation(mutation, in: storage)
             let result = actionHandler(filter, action)
