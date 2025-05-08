362a187d442807c43135af89b45c6ca8f2d1fd91
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-08 07:20:25 -0400

    Beginning to delete old stuff

--- Sources/TextFormation/CompositeFilter.swift
+++ Sources/TextFormation/CompositeFilter.swift
@@ -1,36 +1,6 @@
 import Foundation
+
 import Rearrange
-import TextStory
-
-public struct CompositeFilter {
-    public typealias SubfilterHandler = (Filter, FilterAction) -> FilterAction
-
-    public let filters: [Filter]
-
-    public var actionHandler: SubfilterHandler
-
-    public init(filters: [Filter], handler: @escaping SubfilterHandler = { $1 }) {
-        self.filters = filters
-        self.actionHandler = handler
-    }
-}
-
-extension CompositeFilter: Filter {
-    public func processMutation(_ mutation: TextMutation, in interface: TextInterface, with providers: WhitespaceProviders) -> FilterAction {
-        for filter in filters {
-			let action = filter.processMutation(mutation, in: interface, with: providers)
-            let result = actionHandler(filter, action)
-
-            if action == .none && result == .none {
-                continue
-            }
-
-            return result
-        }
-
-        return .none
-    }
-}
 
 @available(macOS 13.0.0, *)
 public struct NewCompositeFilter<Interface: TextSystemInterface> {
@@ -52,5 +22,4 @@ extension NewCompositeFilter: NewFilter {
 
 		return nil
 	}
-
 }
--- Tests/TextFormationTests/ConsecutiveCharacterRecognizerTests.swift
+++ Tests/TextFormationTests/ConsecutiveCharacterRecognizerTests.swift
@@ -1,76 +1,7 @@
-import XCTest
-import TextStory
-@testable import TextFormation
-
-class ConsecutiveCharacterRecognizerTests: XCTestCase {
-    func testMatching() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "a", at: 0, limit: 0)))
-        XCTAssertEqual(recognizer.state, .tracking(1, 1))
-        XCTAssertFalse(recognizer.processMutation(TextMutation(insert: "b", at: 1, limit: 1)))
-        XCTAssertEqual(recognizer.state, .tracking(2, 2))
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "c", at: 2, limit: 2)))
-        XCTAssertEqual(recognizer.state, .triggered(3))
-    }
-
-    func testMatchingWithMultiCharacterMutations() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "a", at: 0, limit: 0)))
-        XCTAssertEqual(recognizer.state, .tracking(1, 1))
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "bc", at: 1, limit: 1)))
-        XCTAssertEqual(recognizer.state, .triggered(3))
-    }
-
-    func testMatchingAfterDuplicatePrefix() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "a", at: 0, limit: 0)))
-        XCTAssertEqual(recognizer.state, .tracking(1, 1))
-        XCTAssertFalse(recognizer.processMutation(TextMutation(insert: "a", at: 1, limit: 1)))
-        XCTAssertEqual(recognizer.state, .tracking(2, 1))
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "bc", at: 2, limit: 2)))
-        XCTAssertEqual(recognizer.state, .triggered(4))
-    }
-
-    func testMatchingWithSingleMultiCharacterMutation() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertTrue(recognizer.processMutation(TextMutation(insert: "abc", at: 0, limit: 0)))
-        XCTAssertEqual(recognizer.state, .triggered(3))
-    }
-
-    func testMatchingWithReplacementMutation() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertTrue(recognizer.processMutation(TextMutation(string: "abc", range: NSRange(0..<3), limit: 3)))
-        XCTAssertEqual(recognizer.state, .triggered(3))
-    }
-
-    func testMatchingMutationWithMoreCharacters() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertFalse(recognizer.processMutation(TextMutation(string: "abcd", range: NSRange(0..<0), limit: 0)))
-        XCTAssertEqual(recognizer.state, .idle)
-    }
-
-    func testNonMatchingMutationWithMoreCharacters() {
-        let recognizer = ConsecutiveCharacterRecognizer(matching: "abc")
-
-        XCTAssertEqual(recognizer.state, .idle)
-        XCTAssertFalse(recognizer.processMutation(TextMutation(string: "def", range: NSRange(0..<0), limit: 0)))
-        XCTAssertEqual(recognizer.state, .idle)
-    }
-}
-
+import Foundation
 import Testing
+
+import TextFormation
 
 struct NewConsecutiveCharacterRecognizerTests {
 	@Test func matching() throws {
