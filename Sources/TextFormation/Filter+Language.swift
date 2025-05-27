fe34b7f62576f467a7f655f86900ea322932c48a
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-27 06:48:00 -0400

    Expand availability guard to all platforms

--- Sources/TextFormation/Filter+Language.swift
+++ Sources/TextFormation/Filter+Language.swift
@@ -1,5 +1,5 @@
 public enum LanguageFilters {
-	@available(macOS 13.0.0, *)
+	@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
 	public static func generic<Interface: TextFormation.TextSystemInterface>() -> [any Filter<Interface>] {
 		[
 			StandardOpenPairFilter(open: "(", close: ")"),
